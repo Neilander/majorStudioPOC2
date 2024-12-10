cafbd8243bc273ef5404d77fff26b80bb666d516
Neilander <yw6472@nyu.edu>
2024-12-10 01:28:33 -0500

    Start text

---
+++ MajorStudioPoc2/Assets/textGenerator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textGenerator : MonoBehaviour
+{
+    public List<Vector2> textPos;
+    public GameObject textPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
